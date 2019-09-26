301d85238884049c25307fe67019e194433cc644
Solange Gonzalez <solangegf@gmail.com>
2019-09-26 17:40:52 -0300

    Update AlarmEditor.jsx

--- src/components/Alarms/AlarmEditor.jsx
+++ src/components/Alarms/AlarmEditor.jsx
@@ -10,37 +10,41 @@ class AlarmForm extends React.Component {
     return (
       <Form layout="vertical" hideRequiredMark>
         <Row gutter={16}>
-          <Col span={12}>
-            <Form.Item label="Name">
-              {getFieldDecorator('name', {
-                rules: [{ required: true, message: 'Please enter alarm name' }],
-              })(<Input placeholder="Please enter alarm name" />)}
+          <Col span={24}>
+            <Form.Item label="Condition">
+              {getFieldDecorator('termA', {
+                rules: [{ required: true, message: 'Please select a term' }],
+              })(
+                <Select placeholder="Please select a term">
+                  <Option value="binance">Crossing</Option>
+                  <Option value="bitfinex">Greater than</Option>
+                </Select>
+              )}
             </Form.Item>
           </Col>
-          <Col span={12}>
-            <Form.Item label="Exchange">
-              {getFieldDecorator('exchange', {
-                rules: [{ required: true, message: 'Please select an exchange' }],
+          <Col span={24}>
+            <Form.Item>
+              {getFieldDecorator('type', {
+                rules: [{ required: true, message: 'Please select an operator' }],
               })(
-                <Select placeholder="Please select an exchange">
-                  <Option value="binance">Binance</Option>
-                  <Option value="bitfinex">Bitfinex</Option>
+                <Select placeholder="Please select an operator">
+                  <Option value="binance">Crossing</Option>
+                  <Option value="bitfinex">Greater than</Option>
                 </Select>,
               )}
             </Form.Item>
           </Col>
-        </Row>
-        <Row gutter={16}>
+
           <Col span={24}>
-            <Form.Item label="API Key">
-              {getFieldDecorator('apikey', {
-                rules: [
-                  {
-                    required: true,
-                    message: 'please enter your API Key',
-                  },
-                ],
-              })(<Input.TextArea rows={4} placeholder="please enter your API Key" />)}
+            <Form.Item>
+              {getFieldDecorator('termB', {
+                rules: [{ required: true, message: 'Please select an term' }],
+              })(
+                <Select placeholder="Please select an term">
+                  <Option value="value">Binance</Option>
+                  <Option value="vol (20)">Bitfinex</Option>
+                </Select>,
+              )}
             </Form.Item>
           </Col>
         </Row>
