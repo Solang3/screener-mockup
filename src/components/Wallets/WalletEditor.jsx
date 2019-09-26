2ec8aba412fee3ad5d3f20750155cd46aed8ad16
Solange Gonzalez <solangegf@gmail.com>
2019-09-26 17:07:41 -0300

    Update WalletEditor.jsx

--- src/components/Wallets/WalletEditor.jsx
+++ src/components/Wallets/WalletEditor.jsx
@@ -13,88 +13,34 @@ class WalletForm extends React.Component {
           <Col span={12}>
             <Form.Item label="Name">
               {getFieldDecorator('name', {
-                rules: [{ required: true, message: 'Please enter user name' }],
-              })(<Input placeholder="Please enter user name" />)}
+                rules: [{ required: true, message: 'Please enter wallet name' }],
+              })(<Input placeholder="Please enter wallet name" />)}
             </Form.Item>
           </Col>
           <Col span={12}>
-            <Form.Item label="Url">
-              {getFieldDecorator('url', {
-                rules: [{ required: true, message: 'Please enter url' }],
+            <Form.Item label="Exchange">
+              {getFieldDecorator('exchange', {
+                rules: [{ required: true, message: 'Please select an exchange' }],
               })(
-                <Input
-                  style={{ width: '100%' }}
-                  addonBefore="http://"
-                  addonAfter=".com"
-                  placeholder="Please enter url"
-                />,
-              )}
-            </Form.Item>
-          </Col>
-        </Row>
-        <Row gutter={16}>
-          <Col span={12}>
-            <Form.Item label="Owner">
-              {getFieldDecorator('owner', {
-                rules: [{ required: true, message: 'Please select an owner' }],
-              })(
-                <Select placeholder="Please select an owner">
-                  <Option value="xiao">Xiaoxiao Fu</Option>
-                  <Option value="mao">Maomao Zhou</Option>
+                <Select placeholder="Please select an exchange">
+                  <Option value="binance">Binance</Option>
+                  <Option value="bitfinex">Bitfinex</Option>
                 </Select>,
-              )}
-            </Form.Item>
-          </Col>
-          <Col span={12}>
-            <Form.Item label="Type">
-              {getFieldDecorator('type', {
-                rules: [{ required: true, message: 'Please choose the type' }],
-              })(
-                <Select placeholder="Please choose the type">
-                  <Option value="private">Private</Option>
-                  <Option value="public">Public</Option>
-                </Select>,
-              )}
-            </Form.Item>
-          </Col>
-        </Row>
-        <Row gutter={16}>
-          <Col span={12}>
-            <Form.Item label="Approver">
-              {getFieldDecorator('approver', {
-                rules: [{ required: true, message: 'Please choose the approver' }],
-              })(
-                <Select placeholder="Please choose the approver">
-                  <Option value="jack">Jack Ma</Option>
-                  <Option value="tom">Tom Liu</Option>
-                </Select>,
-              )}
-            </Form.Item>
-          </Col>
-          <Col span={12}>
-            <Form.Item label="DateTime">
-              {getFieldDecorator('dateTime', {
-                rules: [{ required: true, message: 'Please choose the dateTime' }],
-              })(
-                <DatePicker.RangePicker
-                  style={{ width: '100%' }}
-                  getPopupContainer={trigger => trigger.parentNode}
-                />,
               )}
             </Form.Item>
           </Col>
         </Row>
         <Row gutter={16}>
           <Col span={24}>
-            <Form.Item label="Description">
-              {getFieldDecorator('description', {
+            <Form.Item label="API Key">
+              {getFieldDecorator('apikey', {
                 rules: [
                   {
                     required: true,
-                    message: 'please enter url description',
+                    message: 'please enter your API Key',
                   },
                 ],
-              })(<Input.TextArea rows={4} placeholder="please enter url description" />)}
+              })(<Input.TextArea rows={4} placeholder="please enter your API Key" />)}
             </Form.Item>
           </Col>
         </Row>
